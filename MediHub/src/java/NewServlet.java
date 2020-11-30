4c12a888b65f88d8bcd8354579e4687f56e3fe76
Tharshan <tharshan24@gmail.com>
2020-11-30 15:10:56 +0530

    no message

--- MediHub/src/java/NewServlet.java
+++ MediHub/src/java/NewServlet.java
@@ -45,7 +45,7 @@ public class NewServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+//     <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
