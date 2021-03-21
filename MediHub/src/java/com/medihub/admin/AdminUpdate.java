b57cc1bc95f432f6b549bb3cacf564a1397fead0
Ifra202 <56605880+Ifra202@users.noreply.github.com>
2021-03-21 14:04:24 +0530

    Merge

--- MediHub/src/java/com/medihub/admin/AdminUpdate.java
+++ MediHub/src/java/com/medihub/admin/AdminUpdate.java
@@ -37,32 +37,25 @@ public class AdminUpdate extends HttpServlet {
             
         try
         {
-            //date and time foramtting
-//            java.util.Date date = Calendar.getInstance().getTime();  
-//            DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");  
-//            String strDate = dateFormat.format(date);
-            
-            //getting from DbConfig class
+
             DbConfig db = DbConfig.getInstance();
             Connection con = db.getConnecton();
                     
             String first_name=request.getParameter("first_name");
             String last_name=request.getParameter("last_name");
             String display_name=request.getParameter("display_name");
-//            String nic=request.getParameter("nic_no");
-//            String dob=request.getParameter("dob");
-//            String gender=request.getParameter("gender");
-//            String email=request.getParameter("email");
-//            String password=request.getParameter("password");
+//          String nic=request.getParameter("nic_no");
+//          String dob=request.getParameter("dob");
+//          String gender=request.getParameter("gender");
+//          String email=request.getParameter("email");
+//          String password=request.getParameter("password");
             String address1=request.getParameter("address_1");
             String address2=request.getParameter("address_2");
             String mobile=request.getParameter("mobile_number");
             String land_line=request.getParameter("land_number");
-//            int district=Integer.parseInt(request.getParameter("district"));
+//          int district=Integer.parseInt(request.getParameter("district"));
             int city=Integer.parseInt(request.getParameter("city"));
-//            int type=Integer.parseInt(request.getParameter("type"));
-//            String zip_code=request.getParameter("zip_code");
-            
+
             
             
             String query="UPDATE users SET "
@@ -81,9 +74,6 @@ public class AdminUpdate extends HttpServlet {
                     + "updated_by="+adminId+","
                     + "updated_at=CURRENT_TIMESTAMP "
                     + "where id="+adminId;
-//                    + "'"+first_name+"','"+last_name+"','"+display_name+"','"+nic+"','"+dob+"','"+gender+"','"+email+"','"+address1+"','"+address2+"',"+city+",'"+mobile+"','"+land_line+"','"+password+"',"+type+",1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
-            
-//            out.print(query);
 
             PreparedStatement stmt=con.prepareStatement(query);  
             int rs=stmt.executeUpdate();
@@ -101,6 +91,6 @@ public class AdminUpdate extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
