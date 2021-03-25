4c5f3d5bf6412f08e03c31e12bae0aaac4f2b4c8
Yashithi98 <2018cs040@stu.ucsc.cmb.ac.lk>
2021-03-25 20:03:27 +0530

    Pharmacy Dashboard Updated

--- MediHub/src/java/com/medihub/pharmacy/PharmacyDashboard.java
+++ MediHub/src/java/com/medihub/pharmacy/PharmacyDashboard.java
@@ -73,7 +73,7 @@ public class PharmacyDashboard extends HttpServlet {
                 request.setAttribute("absolutePath",absolutePath);
                 request.setAttribute("orders", p.getAllOrders()); //directly get all order details
                 request.setAttribute("completedOrders", p.getCompletedOrders()); //directly get all completed order details
-                //out.println(p.getPendingOrders());
+                out.println(p.getCompletedOrders());
                 request.setAttribute("pendingOrders", p.getPendingOrders()); //directly get all pending order details
                 request.getRequestDispatcher("pharmacyDashboard.jsp").forward(request, response);
                 }
